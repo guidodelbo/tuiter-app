c5c0789c7a0f12a842c3731cce38d2feb8bc64a5
Guido Delbo <gdelbo@gmail.com>
2023-01-29 17:52:54 -0300

    added site layout integration and app helper tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = 'Tuiter'
-  end
-
   test 'should get home' do
     get root_path
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', full_title('Home')
   end
 
   test 'should get help' do
     get help_path
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', full_title('Help')
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', full_title('About')
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select 'title', full_title('Contact')
   end
 end
