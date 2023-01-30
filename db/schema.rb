908fd090675f9cbdf211e57715933a7241e6aa65
Guido Delbo <gdelbo@gmail.com>
2023-01-30 20:20:55 -0300

    added user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_29_235524) do
+ActiveRecord::Schema.define(version: 2023_01_30_225046) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
