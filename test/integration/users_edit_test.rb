986906f864827cd2cd097d7b65fa46ffe1d0ccc0
Guido Delbo <gdelbo@gmail.com>
2024-02-26 19:08:39 -0300

    added friendly forwarding to logged in before filter

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
-    log_in_as(@user)
-    get edit_user_path(@user)
   end
 
   test 'unsuccessful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
     assert_template 'users/edit'
 
     patch user_path(@user), params: { user: { name: '',
@@ -18,13 +18,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', text: /the form contains 4 errors/i
   end
 
-  test 'successful edit' do
+  test 'successful edit with friendly forwarding' do
+    # fails because user is not logged in
+    get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
+
     name = 'New Name'
     email = 'new@email.com'
     patch user_path(@user), params: { user: { name: name,
                                               email: email,
                                               password: '',
                                               password_confirmation: '' } }
+
     assert_not flash.empty?
     assert_select 'div.alert', count: 0
     assert_redirected_to @user
