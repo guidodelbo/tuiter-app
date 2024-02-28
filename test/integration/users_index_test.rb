6232fc577f73329aa9b17c9aa936a5b6cd0a9f32
Guido Delbo <gdelbo@gmail.com>
2024-02-28 20:31:44 -0300

    admin users and ability to delete other users from users index

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,18 +2,32 @@ require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @admin_user = users(:michael)
+    @non_admin_user = users(:natalie)
   end
 
-  test 'index includes pagination' do
-    log_in_as(@user)
+  test 'index as admin includes pagination and delete links' do
+    log_in_as(@admin_user)
     get users_path
 
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
 
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin_user
     end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin_user)
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as(@non_admin_user)
+    get users_path
+
+    assert_select 'a', text: 'delete', count: 0
   end
 end
