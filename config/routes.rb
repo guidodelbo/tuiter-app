c67e940667c03dcc04ad3f7190ba13e476c8b0b0
Guido Delbo <gdelbo@gmail.com>
2024-03-17 14:04:36 -0300

    follow/unfollow web interface

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,14 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: %i[edit]
   resources :password_resets, only: %i[new create edit update]
   resources :microposts, only: %i[create destroy]
   get '/microposts', to: 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :relationships, only: %i[create destroy]
 end
