ff9388d73e459f446e0ce75628c2bd2258f11076
Guido Delbo <gdelbo@gmail.com>
2025-04-01 23:24:07 -0700

    fixed postgres config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,18 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # Wait for PostgreSQL to be ready
-until pg_isready -h $DATABASE_HOST -p $DATABASE_PORT -U $DATABASE_USERNAME
+echo "Waiting for PostgreSQL..."
+until pg_isready -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USERNAME" -d "$DATABASE_NAME"
 do
+  echo "db:$DATABASE_PORT - no response"
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
 echo "PostgreSQL is ready!"
 
-# Setup the database if it doesn't exist
-bundle exec rails db:prepare
+# Create and migrate the database
+echo "Running database migrations..."
+bundle exec rails db:migrate RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile)
 exec "$@"
